0ceeda787dbd0ccee420d4e83cb0711315179868
Felipe_Solano <felipe.solano@solera.com>
2022-09-07 09:45:48 -0500

    Adding changes to check Jenkins Job

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo desde Jenkins!!!");
+        System.out.println("This is another change to the Remote Repository...");
+        System.out.println("Hi Again!");
     }
 }
